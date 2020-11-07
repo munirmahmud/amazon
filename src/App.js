3fcf754a2ed04da1e7e5adbe4c7c5ce6ed934b11
Munir Mahmud <munirmahmud6@gmail.com>
2020-11-07 17:05:53 +0600

    Home component declared

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Home from './pages/Home';
 
 const App = () => {
     return (
         <Router>
             <Header />
+            <Home />
         </Router>
     )
 }
